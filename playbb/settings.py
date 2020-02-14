5fceb58faf122ec3b169ddebd473e163463a80d4
kmoskal <moskal.krzysztof@icloud.com>
2020-02-14 15:09:37 +0100

    and saved settings file ;)

--- playbb/settings.py
+++ playbb/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 LOGIN_REDIRECT_URL = 'dashboard:dashboard-home'
 #LOGOUT_REDIRECT_URL = 'accounts:account-logout'
