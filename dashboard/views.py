7146125b29523a82506311c203454aef51237a37
kmoskal <moskal.krzysztof@icloud.com>
2020-05-03 12:46:58 +0200

    add inwentaryzacja-stats to dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ import datetime
 import pandas as pd
 from django_pandas.io import read_frame
 from akcesoria.models import Akcesoria
+from inwentaryzacja.models import Raport
 
 
 
@@ -11,8 +12,12 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = 'accounts:account-login'
     template_name = 'dashboard/dashboard_home.html'
 
+
+
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
+
+        # context do akcesorii
         this_month = datetime.datetime.now().month
         queryset = Akcesoria.objects.filter(data__month=this_month)
         df = read_frame(queryset)
@@ -21,4 +26,27 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         slownik = wartosc.sort_values(ascending=False).to_dict()
         context['akcesoria'] = slownik
         context['suma'] = df['kwota'].sum()
+
+        # context do inwentaryzacji
+        def check_difference(x, y):
+            if x and y:
+                if y-x > 0:
+                    return y-x
+                elif y-x < 0:
+                    return y-x
+                else:
+                    return "OK"
+            else:
+                return "Brak danych"
+
+        qs = Raport.objects.last()
+        context['telefony'] = check_difference(qs.telefony_elza, qs.telefony_stan)
+        context['voice'] = check_difference(qs.voice_elza, qs.voice_stan)
+        context['starter_data'] = check_difference(qs.data_elza, qs.data_stan)
+        context['zdrapki'] = check_difference(qs.zdrapki_elza, qs.zdrapki_stan)
+        context['doladowania'] = check_difference(qs.doladowania_elza, qs.doladowania_stan)
+        context['gotowka'] = check_difference(qs.kasa_elza, qs.kasa_stan)
+        context['data'] = qs.data_raportu
+
+
         return context
