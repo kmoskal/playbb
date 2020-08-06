95f193b3b67e5269f879add276adb91c5e55ce2c
kmoskal <moskal.krzysztof@icloud.com>
2020-08-06 14:21:33 +0200

    change max_length in magazyn->model->symbol(50); nazwa(250); searchstring(250);

--- magazyn/models.py
+++ magazyn/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class StanAkcesoria(models.Model):
-    symbol = models.CharField(max_length=15)
-    nazwa = models.CharField(max_length=150)
+    symbol = models.CharField(max_length=50)
+    nazwa = models.CharField(max_length=250)
     ilosc = models.PositiveSmallIntegerField()
-    searchstring = models.CharField(max_length=150, default="Null")
+    searchstring = models.CharField(max_length=250, default="Null")
 
 
     def __str__(self):
